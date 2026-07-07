17e6d9a66f5c3fb27db54d3762b70d27d3a18b6c
nishant18084 <nishantkumar749204@gmail.com>
2026-07-07 10:52:39 +0530

    Create games-data.js

---
+++ games-data.js
@@ -0,0 +1,7 @@
+const gamesList = [
+    { name: "Subway Surfers", link: "https://m.gamesnacks.com/games/subwaysurfers", icon: "fa-running" },
+    { name: "Snake Arena", link: "https://m.gamesnacks.com/games/snake", icon: "fa-ghost" },
+    { name: "Tower Builder", link: "https://m.gamesnacks.com/games/towerbuilding", icon: "fa-building" },
+    { name: "Color Burst", link: "https://m.gamesnacks.com/games/colorburst", icon: "fa-palette" }
+];
+
