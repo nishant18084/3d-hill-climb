e3586c485f9fc8db56c7b28b26179409ee122f6e
nishant18084 <nishantkumar749204@gmail.com>
2026-07-07 15:04:33 +0530

    Delete movies-data.js

--- movies-data.js
+++
@@ -1,5 +0,0 @@
-const movieStreams = [
-    { name: "Jana Nayagan (2026)", url: "https://rec.ustream.in/sadhna/live/playlist.m3u8", icon: "fa-mask" },
-    { name: "Hollywood Sci-Fi Extra", url: "https://ddnews.akamaized.net/hls/live/2034336/ddnews/master.m3u8", icon: "fa-rocket" },
-    { name: "Bollywood Hits", url: "https://pubads.g.doubleclick.net/ssai/event/O-M2XAnuR3WbQGv9a3B8Xw/master.m3u8", icon: "fa-heart" }
-];
